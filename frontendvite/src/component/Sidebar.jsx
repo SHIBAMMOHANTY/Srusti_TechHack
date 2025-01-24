6934ed4daee19b3e1261370ce32d814028326f9a
jyotiranjan862 <dhaljyotiranjan995@gmail.com>
2025-01-24 18:16:11 +0530

    dashboard

--- frontendvite/src/component/Sidebar.jsx
+++ frontendvite/src/component/Sidebar.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="w-60 bg-gray-800 text-white h-full p-4">
-      <ul className="space-y-4">
-        <li><Link to="/dashboard/profile">Profile</Link></li>
-        <li><Link to="/dashboard/settings">Settings</Link></li>
-        <li><Link to="/dashboard/analytics">Analytics</Link></li>
+    <div className="w-52 bg-gray-800 text-white h-full p-4">
+      <ul className="space-y-8 text-lg font-medium tracking-wide ">
+        <Link to="/dashboard/analytics" className="flex items-center gap-2">
+           Analytics
+        </Link>
+        <li>
+          <Link to="/dashboard/adddata">Add Data</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/reports">Reports</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/survey">Surveys</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/settings">Settings</Link>
+        </li>
       </ul>
     </div>
   );
---
+++ frontendvite/src/pages/Dashboard/AddData.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BiData } from 'react-icons/bi';
+
+const AddData = () => {
+  return (
+    <div>  <BiData size={32} /></div>
+  )
+}
+
+export default AddData
